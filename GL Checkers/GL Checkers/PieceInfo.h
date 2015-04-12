e1b1e62828dcb3f1d18b89e36ce5e49a40aaf835
Kevin <dragn194@gmail.com>
2015-04-11 23:31:09 -0400

    Remove broken file from project

--- GL Checkers/GL Checkers/PieceInfo.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "PieceColor.h"
-
-struct SavedPiece { //Represents one piece saved in a square
-	PieceColor color; //0 is white, 1 is black
-	bool king; //0 is regular, 1 is king
-};
