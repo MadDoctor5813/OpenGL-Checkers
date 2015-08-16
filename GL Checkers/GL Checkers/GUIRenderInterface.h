9197dc7d4180fe3875cd83e2baa7691ef96e545a
Kevin <dragn194@gmail.com>
2015-08-16 13:01:43 -0400

    Add virtual specifier to overridden functions, as its good style.

--- GL Checkers/GL Checkers/GUIRenderInterface.h
+++ GL Checkers/GL Checkers/GUIRenderInterface.h
@@ -10,17 +10,17 @@ public:
 	GUIRenderInterface(App& app);
 	~GUIRenderInterface();
 
-	void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation);
-	Rocket::Core::CompiledGeometryHandle CompileGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture);
-    void RenderCompiledGeometry(Rocket::Core::CompiledGeometryHandle geometry, const Rocket::Core::Vector2f& translation);
-	void ReleaseCompiledGeometry(Rocket::Core::CompiledGeometryHandle geometry);
+	virtual void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation);
+	//Rocket::Core::CompiledGeometryHandle CompileGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture);
+	//void RenderCompiledGeometry(Rocket::Core::CompiledGeometryHandle geometry, const Rocket::Core::Vector2f& translation);
+	//void ReleaseCompiledGeometry(Rocket::Core::CompiledGeometryHandle geometry);
 
-	void EnableScissorRegion(bool enable);
-	void SetScissorRegion(int x, int y, int width, int height);
+	virtual void EnableScissorRegion(bool enable);
+	virtual void SetScissorRegion(int x, int y, int width, int height);
 
-	bool LoadTexture(Rocket::Core::TextureHandle& texture_handle, Rocket::Core::Vector2i& texture_dimensions, const Rocket::Core::String& source);
-	bool GenerateTexture(Rocket::Core::TextureHandle& texture_handle, const Rocket::Core::byte* source, const Rocket::Core::Vector2i& source_dimensions);
-	void ReleaseTexture(Rocket::Core::TextureHandle texture_handle);
+	virtual bool LoadTexture(Rocket::Core::TextureHandle& texture_handle, Rocket::Core::Vector2i& texture_dimensions, const Rocket::Core::String& source);
+	virtual bool GenerateTexture(Rocket::Core::TextureHandle& texture_handle, const Rocket::Core::byte* source, const Rocket::Core::Vector2i& source_dimensions);
+	virtual void ReleaseTexture(Rocket::Core::TextureHandle texture_handle);
 
 private:
 	App& appRef;
